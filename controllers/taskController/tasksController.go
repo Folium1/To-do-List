d96f3eb255af73f8698bb702ddf904eaba0e9be8
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-13 20:29:18 +0200

    Refactored task's controller and created controller for users

---
+++ controllers/taskController/tasksController.go
@@ -0,0 +1,92 @@
+package controller
+
+import (
+	"errors"
+	"log"
+
+	parser "todo/controllers"
+	db "todo/db/tasks"
+	dto "todo/dto"
+)
+
+type taskController struct {
+	t db.TaskService // A field of type db.TaskService to interact with the database.
+}
+
+// New returns a new instance of TaskController.
+// It takes a db.TaskService as an argument and sets it to the 't' field.
+func New(db db.TaskService) TaskController {
+	return &taskController{t: db}
+}
+
+// TaskController is an interface that defines methods to manage tasks.
+type TaskController interface {
+	Create(newTask dto.TaskCreateDTO) error
+	Tasks(userId string) ([]dto.TasksDTO, error)
+	DeleteTask(taskId string) error
+	ChangeData(newTaskData dto.UpdateTaskDTO) error
+}
+
+// Adds a new task to the database.
+func (c *taskController) Create(newTaskDTO dto.TaskCreateDTO) error {
+	var newTask db.Task
+	err := parser.TaskDTOtoDB(newTaskDTO, &newTask)
+	if err != nil {
+		log.Printf("Coudn't parse from dto to db data,err: %v", err)
+		return err
+	}
+	err = c.t.Create(newTask)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Retrieves a list of tasks from the database.
+func (c *taskController) Tasks(userId string) ([]dto.TasksDTO, error) {
+	tasks, err := c.t.Tasks(userId)
+	if err != nil {
+		return nil, err
+	}
+	tasksDTO := make([]dto.TasksDTO, 0, len(tasks))
+	for _, task := range tasks {
+		// Parse data from db.Task struct into dto.TasksDTO struct
+		var taskDTO dto.TasksDTO
+		err = parser.TaskDBtoDTO(task, &taskDTO)
+		tasksDTO = append(tasksDTO, taskDTO)
+	}
+	return tasksDTO, nil
+}
+
+// Deletes a task with the given Id from the database.
+func (c *taskController) DeleteTask(taskId string) error {
+	return c.t.DeleteTask(taskId)
+}
+
+// Updates a task with new data in the database.
+func (c *taskController) ChangeData(newTaskData dto.UpdateTaskDTO) error {
+	task, err := c.t.GetTask(newTaskData.Id) // Retrieves the task from the database using the Id.
+	if err != nil {
+		return err
+	}
+
+	// Update the task fields if they are different from the new data.
+	if task.Deadline != newTaskData.Deadline {
+		task.Deadline = newTaskData.Deadline
+	}
+
+	if task.Description != newTaskData.Description {
+		task.Description = newTaskData.Description
+	}
+
+	if len(task.Description) < 3 {
+		return errors.New("description is too short")
+	}
+
+	// Save the updated task in the database.
+	err = c.t.SaveTask(task)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ controllers/userController/userController.go
@@ -0,0 +1,59 @@
+package usercontroller
+
+import (
+	"log"
+	parser "todo/controllers"
+	db "todo/db/users"
+	"todo/dto"
+)
+
+type UserController struct {
+	db db.Service
+}
+
+type ControllerService interface {
+	Create(newUser dto.UserDTO) (string, error)
+	GetUser(user dto.LoginUserDTO) (dto.LoginUserDTO, error)
+}
+
+func New(userService db.Service) ControllerService {
+	return &UserController{userService}
+}
+
+func (u *UserController) Create(newUser dto.UserDTO) (string, error) {
+	var dbUser db.User
+	err := parser.UsersDTOtoDB(newUser, &dbUser)
+	if err != nil {
+		log.Printf("Coudn't parse from dto to db data,err: %v", err)
+		return "", err
+	}
+	userId, err := u.db.CreateUser(dbUser)
+	if err != nil {
+		log.Printf("Couldn't create user, err: %v", err)
+		return "", err
+	}
+	return userId, nil
+}
+
+func (u *UserController) GetUser(user dto.LoginUserDTO) (dto.LoginUserDTO, error) {
+	var dbUser db.User
+	err := parser.UsersDTOtoDB(user, &dbUser)
+	if err != nil {
+		log.Printf("Couldn't parse data, err = %v", err)
+		return dto.LoginUserDTO{}, err
+	}
+
+	gotUser, err := u.db.GetUser(dbUser)
+	if err != nil {
+		log.Printf("Couldn't get user from db,err: %v", err)
+		return dto.LoginUserDTO{}, err
+	}
+
+	var gotUserDTO dto.LoginUserDTO
+	err = parser.UsersDBtoDTO(gotUser, &gotUserDTO)
+	if err != nil {
+		log.Printf("Couldn't parse data from db to dto, err: %v", err)
+		return dto.LoginUserDTO{}, err
+	}
+	return gotUserDTO, nil
+}
