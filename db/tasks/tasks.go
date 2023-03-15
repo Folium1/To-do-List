7a71cb670a60bab809b3e637be618199117a63b8
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-15 19:53:10 +0200

    changed queries to db

--- db/tasks/tasks.go
+++ db/tasks/tasks.go
@@ -15,21 +15,22 @@ func NewService() TaskService {
 
 type TaskService interface {
 	Create(newTask Task) error
-	Tasks() ([]Task, error)
+	Tasks(userId string) ([]Task, error)
 	DeleteTask(taskId string) error
 	GetTask(id string) (Task, error)
 	SaveTask(task Task) error
 }
 
 // Returns a slice of all tasks
-func (t *Task) Tasks() ([]Task, error) {
-	db, err := db.DbConnect("/tasks")
+func (t *Task) Tasks(userId string) ([]Task, error) {
+	db, err := db.DbConnect()
 	if err != nil {
 		log.Print(err)
 		return nil, err
 	}
 	defer db.Close()
-	query, err := db.Query("SELECT task_id,description, deadline, is_done FROM tasks ORDER BY deadline;")
+	q := fmt.Sprintf("SELECT task_id,description, deadline, is_done FROM tasks WHERE user_id = '%v' ORDER BY deadline ;", userId)
+	rows, err := db.Query(q)
 	if err != nil {
 		fmt.Println(err)
 		log.Print(err)
@@ -37,9 +38,9 @@ func (t *Task) Tasks() ([]Task, error) {
 	}
 	var allTasks []Task
 	var oneTask Task
-	// Itarate through the query
-	for query.Next() {
-		err := query.Scan(&oneTask.Id, &oneTask.Description, &oneTask.Deadline, &oneTask.Done)
+	// Itarate through the rows
+	for rows.Next() {
+		err := rows.Scan(&oneTask.Id, &oneTask.Description, &oneTask.Deadline, &oneTask.Done)
 		if err != nil {
 			log.Print(err)
 			return nil, err
@@ -52,12 +53,12 @@ func (t *Task) Tasks() ([]Task, error) {
 
 // Adds a new task to the database.
 func (t *Task) Create(newTask Task) error {
-	db, err := db.DbConnect("/tasks")
+	db, err := db.DbConnect()
 	if err != nil {
 		log.Print(err)
 	}
 	defer db.Close()
-	q := fmt.Sprintf("INSERT INTO tasks(description, deadline, is_done) VALUES('%v', '%v', false);", newTask.Description, newTask.Deadline)
+	q := fmt.Sprintf("INSERT INTO tasks(description,user_id,deadline, is_done) VALUES('%v','%v','%v', false);", newTask.Description, newTask.UserId, newTask.Deadline)
 	_, err = db.Query(q)
 	if err != nil {
 		return err
@@ -67,7 +68,7 @@ func (t *Task) Create(newTask Task) error {
 
 // Removes a task from the database.
 func (t *Task) DeleteTask(taskId string) error {
-	db, err := db.DbConnect("/tasks")
+	db, err := db.DbConnect()
 	if err != nil {
 		return err
 	}
@@ -82,19 +83,19 @@ func (t *Task) DeleteTask(taskId string) error {
 
 // Returns task by it's id.
 func (t *Task) GetTask(id string) (Task, error) {
-	db, err := db.DbConnect("/tasks")
+	db, err := db.DbConnect()
 	if err != nil {
 		return Task{}, err
 	}
-	query, err := db.Query(fmt.Sprintf("SELECT description,deadline,is_done FROM tasks WHERE task_id = '%v';", id))
+	rows, err := db.Query(fmt.Sprintf("SELECT description,deadline,is_done FROM tasks WHERE task_id = '%v';", id))
 	if err != nil {
 		return Task{}, err
 	}
 	var task Task
 	task.Id = id
-	// Itarate through the query
-	for query.Next() {
-		err := query.Scan(&task.Description, &task.Deadline, &task.Done)
+	// Itarate through the rows
+	for rows.Next() {
+		err := rows.Scan(&task.Description, &task.Deadline, &task.Done)
 		if err != nil {
 			return Task{}, err
 		}
@@ -107,12 +108,12 @@ func (t *Task) GetTask(id string) (Task, error) {
 
 // Saves updated task's data.
 func (t *Task) SaveTask(task Task) error {
-	db, err := db.DbConnect("/tasks")
+	db, err := db.DbConnect()
 	if err != nil {
 		return err
 	}
 	defer db.Close()
-	q := fmt.Sprintf("UPDATE tasks SET is_done = 0, description= '%v', deadline='%v' WHERE task_id = '%v';", task.Description, task.Deadline, task.Id)
+	q := fmt.Sprintf("UPDATE todo.tasks SET is_done = 0, description= '%v', deadline='%v' WHERE task_id = '%v';", task.Description, task.Deadline, task.Id)
 	_, err = db.Query(q)
 	if err != nil {
 		return err
