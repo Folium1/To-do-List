1a084fd9e07c5ef50810d3956ac01372dd5a07be
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-15 20:51:43 +0200

    Created http paths for user login and sign up

--- handler/handler.go
+++ handler/handler.go
@@ -1,125 +1,39 @@
 package handler
 
 import (
-	"errors"
-	"fmt"
-	"strings"
-
 	"net/http"
 	"text/template"
 
-	"todo/controller"
-	"todo/db"
-	dto "todo/dto"
+	controller "todo/controllers/taskController"
+	usercontroller "todo/controllers/userController"
+	db "todo/db/tasks"
+	"todo/db/users"
 )
 
 var (
 	task           = db.NewService()
 	taskController = controller.New(task)
+	user           = users.New()
+	userController = usercontroller.New(user)
 	templ          *template.Template
 )
 
-// The main function is responsible for rendering the main page.
-func main(w http.ResponseWriter, r *http.Request) {
-	// Retrieve all tasks from the task controller
-	tasks, err := taskController.Tasks()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-	}
-
-	// Pass the tasks to the template for rendering
-	err = templ.ExecuteTemplate(w, "index.html", tasks)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-}
-
-// Redirects to main page.
-func redirectToMain(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/todo", http.StatusMovedPermanently)
-}
-
-// The createTask function is responsible for creating a new task.
-func createTask(w http.ResponseWriter, r *http.Request) {
-	// Retrieve the task data from the HTTP request.
-	if r.Method == "POST" {
-		var newTask dto.TaskCreateDTO
-		newTask.Description = r.PostFormValue("description")
-		if newTask.Description == "" {
-			err := fmt.Sprintf("wrong description: %v", newTask.Description)
-			http.Error(w, err, http.StatusBadRequest)
-		}
-		deadline := r.PostFormValue("date")
-		if deadline == "" {
-			err := fmt.Sprintf("wrong deadline: %v", deadline)
-			http.Error(w, err, http.StatusBadRequest)
-		}
-		newTask.Deadline = deadline
-
-		// Pass the new task data to the task controller for creation.
-		err := taskController.Create(newTask)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-
-		// Redirects the client to the main page.
-		redirectToMain(w, r)
-	}
-
-}
-
-// The updateData function is responsible for updating an existing task.
-func updateData(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		method := r.FormValue("_method")
-		if method == "PATCH" {
-			// Retrieve the task ID and updated data from the HTTP request.
-			id := r.PostFormValue("id")
-			if id == "" {
-				err := errors.New("wrond task id")
-				http.Error(w, err.Error(), http.StatusNotFound)
-			}
-			var newTaskData dto.UpdateTaskDTO
-			newTaskData.Id = id
-			newTaskData.Deadline = r.FormValue("deadline")
-			newTaskData.Description = r.FormValue("description")
-			err := taskController.ChangeData(newTaskData)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
-			// Redirects the client to the main page.
-			redirectToMain(w, r)
-		}
-	}
-}
-
-// The deleteTask function is responsible for the deletion of the task.
-func deleteTask(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	parts := strings.Split(path, "/")
-	id := parts[2]
-	err := taskController.DeleteTask(id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-	}
-	// Redirects the client to the main page.
-	redirectToMain(w, r)
-}
-
-// Starts server.
-func InitServer() {
+func StartServer() {
 	var err error
 	templ, err = template.ParseGlob("templates/*.html")
 	if err != nil {
 		panic(err)
 	}
 
+	http.HandleFunc("/", signUpHandler)
+	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/todo", main)
+
 	http.HandleFunc("/create/", createTask)
 	http.HandleFunc("/update/", updateData)
 	http.HandleFunc("/delete/", deleteTask)
 
-	err = http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":9090", nil)
 	if err != nil {
 		panic(err)
 	}
