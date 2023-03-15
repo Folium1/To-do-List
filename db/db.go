c7d6979fb29146a78a942d01e3b6cf25312f74a6
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-15 20:22:04 +0200

    Changed db tables intialization

--- db/db.go
+++ db/db.go
@@ -9,13 +9,13 @@ import (
 )
 
 // DbConnect establishes a connection to the database.
-func DbConnect(table string) (*sql.DB, error) {
+func DbConnect() (*sql.DB, error) {
 	// Load environment variables from .env file.
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-	dataSourceName := os.Getenv("DB_SOURCE") + table
+	dataSourceName := os.Getenv("DB_SOURCE")
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		return db, err
@@ -25,15 +25,46 @@ func DbConnect(table string) (*sql.DB, error) {
 
 // DbTableInit creates the tasks table in the database if it does not already exist.
 func DbTableInit() error {
-	db, err := DbConnect("")
+	db, err := DbConnect()
 	if err != nil {
 		log.Printf("couldn't connect to db, err: %v", err)
 		return err
 	}
-	_, err = db.Query("CREATE TABLE IF NOT EXISTS tasks (task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,description VARCHAR(50),deadline DATETIME,is_done TINYINT(1));")
+	
+	// creating todo schema
+	_, err = db.Query("CREATE SCHEMA IF NOT EXISTS todo;")
 	if err != nil {
-		log.Printf("Couldn't create table")
+		log.Printf("Couldn't create todo schema")
 		return err
 	}
+
+	// creating users table
+	_, err = db.Query("CREATE TABLE IF NOT EXISTS todo.users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,user_name VARCHAR(50),mail VARCHAR(50) NOT NULL UNIQUE,password VARCHAR(200));")
+	if err != nil {
+		log.Printf("Couldn't create users table")
+		return err
+	}
+
+	// creating idx_user_id if it is not created
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM information_schema.statistics WHERE table_name ='users' AND index_name = 'idx_user_id'").Scan(&count)
+	if err != nil {
+		log.Printf("Error checking if index exists: %v", err)
+		return err
+	}
+	if count == 0 {
+		_, err = db.Query("CREATE INDEX idx_user_id ON todo.users(user_id);")
+		if err != nil {
+			log.Printf("Couldn't create index")
+			return err
+		}
+	}
+	// creting tasks table
+	_, err = db.Query("CREATE TABLE IF NOT EXISTS todo.tasks(task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,user_id INT,description VARCHAR(50),deadline DATETIME,is_done TINYINT(1),CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id));")
+	if err != nil {
+		log.Printf("Couldn't create tasks table")
+		return err
+	}
+
 	return nil
 }
